75ddacc51acc6270e585749d4006941d45afd7d0
Brett Smith <jbsmith731@gmail.com>
2023-03-29 15:43:57 -0500

    Refactor: move tailwind config to TS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-const createFluidValue = require('./createFluidValue');
+import { createFluidValue } from '~/style/createFluidValue';
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
