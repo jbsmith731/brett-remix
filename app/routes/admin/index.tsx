44f11a693a57cb5a950bcddd3318a5349bf1b807
Brett Smith <jbsmith731@gmail.com>
2023-04-04 12:12:38 -0500

    Add: Radix form

--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -1,4 +1,5 @@
-import { Form, useActionData } from '@remix-run/react';
+import * as Form from '@radix-ui/react-form';
+import { Form as RemixForm, useActionData } from '@remix-run/react';
 import type { ActionArgs } from '@vercel/remix';
 import { json } from '@vercel/remix';
 import { z } from 'zod';
@@ -25,9 +26,9 @@ export const action = async ({ request }: ActionArgs) => {
   }
 
   try {
-    bookmarkSchema.parse(formData);
+    const bookmarks = bookmarkSchema.parse(formData);
 
-    await supabase.from('Bookmarks').insert([formData]);
+    await supabase.from('Bookmarks').insert([bookmarks]);
 
     return json({ submitError: null, validationError: null }, { status: 201 });
   } catch (error) {
@@ -38,25 +39,72 @@ export const action = async ({ request }: ActionArgs) => {
 const Admin = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { submitError, validationError } = useActionData<typeof action>() ?? {};
+  console.log(Boolean(validationError?.title));
 
   return (
     <main className={container}>
       <h1>Admin</h1>
 
-      <Form method="post">
-        <input type="text" name="title" placeholder="title" />
-        <input type="text" name="url" placeholder="url" />
-        <textarea name="description" placeholder="description" />
-        <button type="submit">Submit</button>
-      </Form>
+      <Form.Root asChild>
+        <RemixForm method="post">
+          <Form.Field
+            name="title"
+            serverInvalid={Boolean(validationError?.title)}
+          >
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="title" />
+            <Form.Message
+              match="valueMissing"
+              forceMatch={Boolean(validationError?.title)}
+            >
+              {validationError?.title
+                ? validationError.title._errors.join(', ')
+                : null}
+            </Form.Message>
+          </Form.Field>
+
+          <Form.Field name="url" serverInvalid={Boolean(validationError?.url)}>
+            <Form.Label>URL</Form.Label>
+            <Form.Control type="text" placeholder="URL" />
+            <Form.Message
+              match="valueMissing"
+              forceMatch={Boolean(validationError?.url)}
+            >
+              {validationError?.url
+                ? validationError.url._errors.join(', ')
+                : null}
+            </Form.Message>
+          </Form.Field>
+
+          <Form.Field
+            name="description"
+            serverInvalid={Boolean(validationError?.url)}
+          >
+            <Form.Label>Description</Form.Label>
+            <Form.Control placeholder="Description" asChild>
+              <textarea />
+            </Form.Control>
+            <Form.Message
+              match="valueMissing"
+              forceMatch={Boolean(validationError?.description)}
+            >
+              {validationError?.description
+                ? validationError.description._errors.join(', ')
+                : null}
+            </Form.Message>
+          </Form.Field>
+
+          <Form.Submit type="submit">Submit</Form.Submit>
+        </RemixForm>
+      </Form.Root>
     </main>
   );
 };
 
 const bookmarkSchema = z.object({
-  title: z.string().max(240),
-  url: z.string().url(),
-  description: z.string().optional(),
+  title: z.string().max(240).trim(),
+  url: z.string().url().trim(),
+  description: z.string().trim().optional(),
 });
 
 export default Admin;
