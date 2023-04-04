cd26c7405989118b37e4292ac2b7e052b2898c10
Brett Smith <jbsmith731@gmail.com>
2023-04-03 20:54:16 -0500

    Post bookmarks

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -17,18 +17,12 @@ export type SupabaseContext = {
 };
 
 export const loader = async ({ request }: LoaderArgs) => {
-  // environment variables may be stored somewhere other than
-  // `process.env` in runtimes other than node
-  // we need to pipe these Supabase environment variables to the browser
   const env = {
     SUPABASE_URL: process.env.SUPABASE_URL!,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
   };
 
-  // We can retrieve the session on the server and hand it to the client.
-  // This is used to make sure the session is available immediately upon rendering
   const response = new Response();
-
   const supabase = createServerClient({ request, response });
 
   const {
--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -1,4 +1,36 @@
-const Admin = () => {
-  return <div>Admin</div>;
+import type { ActionArgs } from '@vercel/remix';
+import { container } from '~/style/container';
+import { createServerClient } from '~/utils/supabase.server';
+
+export const action = async ({ request }: ActionArgs) => {
+  const response = new Response();
+  const supabase = createServerClient({ request, response });
+  const formData = await request.formData();
+
+  const { data, error } = await supabase.from('Bookmarks').insert([
+    {
+      title: formData.get('title') as string | null,
+      url: formData.get('url') as string | null,
+      description: formData.get('description') as string | null,
+    },
+  ]);
+
+  return { data, error };
 };
+
+const Admin = () => {
+  return (
+    <main className={container}>
+      <h1>Admin</h1>
+
+      <form method="post" action="?index">
+        <input type="text" name="title" placeholder="title" />
+        <input type="text" name="url" placeholder="url" />
+        <textarea name="description" placeholder="description" />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
 export default Admin;
