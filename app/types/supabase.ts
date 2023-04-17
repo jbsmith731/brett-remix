53389612930b71a7d6278831118539f9dd76fbac
Brett Smith <jbsmith731@gmail.com>
2023-04-17 16:21:22 -0500

    Add contact page redirect, update types =(

--- app/types/supabase.ts
+++ app/types/supabase.ts
@@ -4,46 +4,46 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       Bookmarks: {
         Row: {
-          created_at: string | null;
-          description: string | null;
-          id: number;
-          title: string | null;
-          url: string | null;
-        };
+          created_at: string | null
+          description: string | null
+          id: number
+          title: string
+          url: string
+        }
         Insert: {
-          created_at?: string | null;
-          description?: string | null;
-          id?: number;
-          title?: string | null;
-          url?: string | null;
-        };
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          title: string
+          url: string
+        }
         Update: {
-          created_at?: string | null;
-          description?: string | null;
-          id?: number;
-          title?: string | null;
-          url?: string | null;
-        };
-      };
-    };
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          title?: string
+          url?: string
+        }
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
