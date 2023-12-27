cb15f18a5af2fc29152a09f4f53d42624de35104
Brett Smith <jbsmith731@gmail.com>
2023-12-27 11:01:37 -0600

    Chore: Upgrade deps (#12)
    * Chore: Upgrade deps
    
    * Generate new types

--- remix.config.js
+++ remix.config.js
@@ -5,9 +5,12 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   tailwind: true,
+  serverModuleFormat: 'cjs',
   future: {
     v2_normalizeFormMethod: true,
     v2_errorBoundary: true,
     v2_routeConvention: true,
+    v2_dev: true,
+    v2_headers: true,
   },
 };
